040dff9802c864f8e6f05bce046887f0d7a2239c
heshanu <heshanu97@gmail.com>
2023-12-02 18:11:35 +0530

    added updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ErrorMessageComponent } from './shared/error-message/error-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ErrorMessageComponent
   ],
   imports: [
     BrowserModule,
